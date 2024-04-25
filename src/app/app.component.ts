113d69268283e0c87dc5fdb5cdc40b43d2682126
rayyan-mirza <mirzarayyan125@gmail.com>
2024-04-26 03:44:22 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { webSocket } from 'rxjs/webSocket';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chat-app-ui';
+  private subject = webSocket('ws://localhost:8080');
+  constructor(){
+    this.subject.subscribe();
+    // this.subject.subscribe({
+    //   next: msg => console.log('message received: ' + msg), // Called whenever there is a message from the server.
+    //   error: err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+    //   complete: () => console.log('complete') // Called when connection is closed (for whatever reason).
+    // });
+  }
+  sendmsg():void{
+    this.subject.next({message:'front speaking'});
+    // console.log("jalo")
+  }
 }
