641849630b2f3c2252fccb8a7e67a05101f83538
rayyan-mirza <mirzarayyan125@gmail.com>
2024-04-27 13:08:36 +0500

    setting up ui

--- src/app/components/chat-window/chat-window.component.ts
+++ src/app/components/chat-window/chat-window.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-chat-window',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.css'
 })
 export class ChatWindowComponent {
-
+  ischatopen!:boolean;
+  constructor(){
+    this.ischatopen = false;
+  }
 }
--- src/app/components/user-dashboard/searchable-users-list/searchable-users-list.component.ts
+++ src/app/components/user-dashboard/searchable-users-list/searchable-users-list.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-searchable-users-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './searchable-users-list.component.html',
-  styleUrl: './searchable-users-list.component.css'
+  styleUrl: './searchable-users-list.component.css',
 })
 export class SearchableUsersListComponent {
-
+  dummy_users: string[] = [
+    'saad',
+    'anas',
+    'maaz',
+    'saad',
+    'anas',
+    'maaz',
+    'saad',
+    'anas',
+    'maaz',
+    'saad',
+    'anas',
+    'maaz',
+    'saad',
+    'anas',
+    'maaz',
+    'saad',
+    'anas',
+    'maaz',
+  ];
+  // create an array as filtered array and an array as all users
+  // always render the display /filtered array of users
+  on_user_click(user: string): void {
+    console.log('user clicked ==', user);
+  }
 }
